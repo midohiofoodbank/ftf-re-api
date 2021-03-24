f2aebe530a5023b54ec082c78937a8b7fd4776bc
jwhitesmith <whitesmith.2@osu.edu>
2021-03-24 13:45:39 -0400

    Added control types 4 and 5, untested

--- reporting_engine/transform_layer/services/data_service.py
+++ reporting_engine/transform_layer/services/data_service.py
@@ -290,6 +290,10 @@ class Data_Service:
             return f"dim_service_types.service_category_code IN (10, 15)"
         elif (control_type_name == "Produce only"):
             return f"dim_service_types.service_category_code IN (20)"
+        elif (control_type_name == "Everything"):
+            return f"dim_service_types.service_category_code = *"
+        elif (control_type_name == "TEFAP"):
+            return f'dim_service_types.name = "TEFAP"'
         else:
             return f"dim_service_types.dummy_is_grocery_service = 1"
 
